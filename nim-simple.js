c05d13757849a821fc97407f370c5af42a710239
bmoreini <42300498+bmoreini@users.noreply.github.com>
2018-10-05 09:18:58 -0400

    Updated from Wyatt's version

--- nim-simple.js
+++ nim-simple.js
@@ -1,32 +1,36 @@
-// Simple Nim Mr. Moreinis
-// Initializations
+// Nim Trainer Starter
+// Initialzations
 var count = 0;
 var playerInput = 0;
 var cpuInput = 0;
+var winner=null;
 // Welcome
 alert("Welcome to Nim! You go first.");
 // Start Play
-// Keep looping until someone wins
+// Kepp looping until someone wins
 while (count<21) {
-   // ask for player input and say current count, add to count
-   count+=playerTurn(count);
-   // obtain CPU input and say current count, add to count
-   count+=cpuTurn(count);
-   }
-alert("Somebody won!");
-// End
-
-// Define function cpuTurn
-function cpuTurn(count){
-   cpuInput=Math.floor(Math.random()*3+1);
-   console.log(cpuInput);
-   alert("Current count is "+(count+cpuInput));
-   return cpuInput;
+  // Ask for player input and say current count, add to count
+  count+=playerTurn(count);
+alert("Current count is "+count);
+  // Ask for CPU inout and say current count, add to count
+  count+=cpuTurn(count);
 }
+alert(winner+" won!");
+// End
 
 // Define function playerTurn
 function playerTurn(count){
-   playerInput = parseInt(prompt("Count how many? (1-3)"));
-   alert("Current count is "+(count+playerInput));
-   return playerInput;
+  playerInput = prompt("Count how many? (1-3)");
+  count=parseInt(playerInput);
+  winner ="CPU";
+  return count;
+}
+
+// Define function cpuTurn
+function cpuTurn(count){
+  cpuInput=Math.floor(Math.random()*3)+1;
+  count=cpuInput;
+  alert("Current count is "+count);
+  winner="You";
+  return count;
 }
