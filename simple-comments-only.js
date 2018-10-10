da86997a274394d5b3ddea9bceba207e622d1cfd
bmoreini <42300498+bmoreini@users.noreply.github.com>
2018-10-10 12:37:43 -0400

    Full Comments Only for Nim Simple

---
+++ simple-comments-only.js
@@ -0,0 +1,33 @@
+// Nim Simple Comments 
+/* Initialzations */
+// count 0
+// playerInput 0
+// cpuInput 0
+// winner null
+// Welcome - U go first
+/* Start Play */
+// while loop while count is less than 21
+   // set winner as CPU
+   // Call function to ask for player input and say current count, add to count
+   // Alert current count
+   // Check if player loses, and prevent CPU from taking turn if so
+      // Set winner to player
+      // set cpuInput equal to cpu's turn (function)
+      // Alert CPU's count
+      // Add CPUInput to count
+      // Alert current count
+      // Close conditional
+// Close while loop   
+// Say who won (winner)
+// End
+
+// Define function playerTurn
+   // set playerInput into prompted user entry 1-3
+   // convert playerInput string into integer
+   // return playerInput 
+// end function
+
+// Define function cpuTurn
+   // set cpuInput as random number 1-3
+   // return cpuInput
+// end function
