aaf379f01fcb4521bcc68ee03d59890dcf7646a4
bmoreini <42300498+bmoreini@users.noreply.github.com>
2018-10-11 10:34:16 -0400

    Update nim-trainer.js

--- nim-trainer.js
+++ nim-trainer.js
@@ -1,36 +1,55 @@
-// Nim Trainer Starter
+// Nim Trainer by Bram Moreinis
 // Initialzations
 var count = 0;
 var playerInput = 0;
 var cpuInput = 0;
 var winner=null;
+var gameState = "simple"; //Initalize gameState to "Simple" to set default game state to it
 // Welcome
+gameState=prompt("Choose simple or trainer?");
+
 alert("Welcome to Nim! You go first.");
 // Start Play
-// Kepp looping until someone wins
+// Keep looping until someone wins
 while (count<21) {
-  // Ask for player input and say current count, add to count
-  count+=playerTurn(count);
-alert("Current count is "+count);
-  // Ask for CPU inout and say current count, add to count
-  count+=cpuTurn(count);
+   // Ask for player input and say current count, add to count
+   winner="CPU"; 
+   count+=playerTurn();
+   alert("Current count is "+count);
+   // Check if player loses, and prevent CPU from taking turn if so
+   if (count<21) {
+      // Ask for CPU input and say current count, add to count
+      winner="you";
+   if (gameState=="trainer"){
+      cpuInput=cpuTurnTrainer(count);    
+    }
+   else(cpuInput=cpuTurn());
+   alert("CPU counts "+cpuInput);
+   count+=cpuInput;
+   alert("Current count is "+count);
+   }
 }
 alert(winner+" won!");
 // End
 
 // Define function playerTurn
-function playerTurn(count){
+function playerTurn(){
   playerInput = prompt("Count how many? (1-3)");
-  count=parseInt(playerInput);
-  winner ="CPU";
-  return count;
+  playerInput = parseInt(playerInput);
+  return playerInput;
 }
 
 // Define function cpuTurn
-function cpuTurn(count){
+function cpuTurn(){
   cpuInput=Math.floor(Math.random()*3)+1;
-  count=cpuInput;
-  alert("Current count is "+count);
-  winner="You";
-  return count;
+  return cpuInput;
+}
+
+function cpuTurnTrainer(count){
+   var multi=0;
+   for(multi = 4; multi < count; multi += 4) {
+      console.log(multi);
+   }
+   cpuInput = multi - count;     
+   return cpuInput;
 }
