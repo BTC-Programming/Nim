eab1ba349c6c756e6e639b9dd55be0aa94f9708e
bmoreini <42300498+bmoreini@users.noreply.github.com>
2019-11-12 19:06:25 -0500

    Update nim-super-simple.js

--- nim-super-simple.js
+++ nim-super-simple.js
@@ -1,31 +1,34 @@
-var count = 0,player = 0,cpu = 0;
+var count = 0, turn = 0;
 alert("Let\'s play Nim. You go first");
 while(count<21){
 	count += player;
 	if(count < 21){
-		player = playerInput(count);
+		turn = userTurn();
+		count+=turn;
+		alert("Count is now "+count);
+	if (count == 20) alert("Computer lost.");
+	else if (count > 20) alert ("You lost.");
+	else {
+		turn=cpuTurn(count);
+		count += turn;
 		alert("the count is now "+count);
-		if(count == 20){
-			alert("computer lost");
-		} else {
-			cpu = Math.floor(Math.random()*3)+1;
-			count += cpu;
-			if(count>21){
-				count = 20;
-			}
-			alert("the count is now "+count);
-		}
 	} 
 	else {
 		alert("You lost!");
 	}
 }
 
-function playerInput(count) {
-	player = prompt("Enter 1, 2 or 3");
-	if (player !=1 && player!=2 && player !=3) {
+function userTurn() {
+	var turn=0;
+	while (turn !=1 && turn!=2 && turn !=3) {
+		turn = prompt("Enter 1, 2 or 3");
 		alert("Invalid entry.");
-		player = playerInput(count);
 	}
-	return parseInt(player);
+	return Number(turn);
 }
+
+function CPUTurn(count){
+	while (count < 20) var turn = Math.floor(Math.random()*3)+1;
+	return turn;
+}
+	
