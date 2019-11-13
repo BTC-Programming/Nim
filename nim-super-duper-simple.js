44eeacf22a2b8eb6c8fab6bd71643df74bbec191
bmoreini <42300498+bmoreini@users.noreply.github.com>
2019-11-12 19:48:11 -0500

    Update and rename nim-simple.js to nim-super-duper-simple.js

---
+++ nim-super-duper-simple.js
@@ -0,0 +1,25 @@
+// Nim Super Duper Simple (no input validation)
+var count = 0; playerInput = 0; cpuInput = 0; var winner=null;
+alert("Welcome to Nim! You go first.");
+while (count<21) {
+   winner="CPU"; 
+   count+=playerTurn();
+   alert("Current count is "+count);
+   if (count<21) {
+      winner="you"; 
+      count+=cpuTurn();
+      alert("Current count is "+count);
+   }
+}
+alert(winner+" won!");
+
+function playerTurn(){
+  var turn = prompt("Count how many? (1-3)");
+  return Number(turn);
+}
+
+function cpuTurn(){
+  var turn=Math.floor(Math.random()*3)+1;
+  alert("CPU counts "+turn+".");
+  return turn;
+}
